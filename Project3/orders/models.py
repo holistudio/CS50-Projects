2a92f4ea5f626a3f7f6a21445b0291c60c4a9056
holistudio <daniel.bin.lu@gmail.com>
2018-10-11 11:34:20 -0400

    MenuItem, PizzaMenuItem, and ToppingMenuItem

--- Project3/orders/models.py
+++ Project3/orders/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
+SMALL = 'S'
+LARGE = 'L'
+
+SIZE_CHOICES = (
+	(SMALL, "Small"),
+	(LARGE, "Large"),
+)
+
+
 # Create your models here.
+class MenuItem(models.Model):
+	#Price
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+
+	#Item Type (Pizza/Sub/Pasta/Salad/Platter)
+	PIZZA = 'PZA'
+	SUB = 'SUB'
+	PASTA = 'PTA'
+	SALAD = 'SAL'
+	PLATTER = 'PLT'
+
+	ITEM_TYPE_CHOICES = (
+		(PIZZA, "Pizza"),
+		(SUB, "Sub"),
+		(PASTA, "Sub"),
+		(SALAD, "Salad"),
+		(PLATTER, "Platter"),
+	)
+
+	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PIZZA);
+
+	#"toString method"
+	def __str__(self):
+		self.item_type;
+
+#PizzaMenuItem (inherit MenuItem)
+class PizzaMenuItem(MenuItem):
+	#Pizza Type (Regular/Sicilian)
+	REGULAR = 'RGL'
+	SICILIAN = 'SCL'
+
+	PIZZA_TYPE_CHOICES = (
+		(REGULAR, "Regular"),
+		(SICILIAN, "Sicilian"),
+	)
+	#Pizza Size (Small/Large)
+	pizza_size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
+
+	#Topping Selection	(Cheese 0, 1 topping 1, 2 toppings 2, 3 toppings 3, Special 4)
+	TOPPING_SEL_CHOICES = (
+		('C', "Cheese"),
+		('1',"1 topping"),
+		('2',"2 toppings"),
+		('3',"3 toppings"),
+		('S',"Special")
+	)
+	topping_sel = models.CharField(max_length=1, choices = TOPPING_SEL_CHOICES, default = 'C')
+
+#ToppingMenuItem (though it is technically a menu item, a topping doesn't have an associated price or "type")
+class ToppingMenuItem(models.Model)
+	#Topping Name
+	name = models.CharField(max_length=100);
+
