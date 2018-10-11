22ce529055f80be6178af36b54093efaf76af3e7
holistudio <daniel.bin.lu@gmail.com>
2018-10-11 11:54:42 -0400

    Additional MenuItems

--- Project3/orders/models.py
+++ Project3/orders/models.py
@@ -31,9 +31,14 @@ class MenuItem(models.Model):
 
 	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PIZZA);
 
+	item_name = models.CharField(max_length=100);
+
 	#"toString method"
 	def __str__(self):
-		self.item_type;
+		str(f"{self.item_type} - {self.item_name}");
+
+	class Meta:
+		abstract = True
 
 #PizzaMenuItem (inherit MenuItem)
 class PizzaMenuItem(MenuItem):
@@ -45,8 +50,11 @@ class PizzaMenuItem(MenuItem):
 		(REGULAR, "Regular"),
 		(SICILIAN, "Sicilian"),
 	)
+
+	item_name = models.CharField(max_length=3, choicese = PIZZA_TYPE_CHOICES, default = REGULAR)
+
 	#Pizza Size (Small/Large)
-	pizza_size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
+	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
 
 	#Topping Selection	(Cheese 0, 1 topping 1, 2 toppings 2, 3 toppings 3, Special 4)
 	TOPPING_SEL_CHOICES = (
@@ -63,3 +71,14 @@ class ToppingMenuItem(models.Model)
 	#Topping Name
 	name = models.CharField(max_length=100);
 
+class SubMenuItem(MenuItem):
+	#Sub Size (Small/Large)
+	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
+
+class SaladMenuItem(MenuItem):
+
+class PastaMenuItem(MenuItem):
+
+class PlatterMenuItem(MenuItem):
+	#Platter Size (Small/Large)
+	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
