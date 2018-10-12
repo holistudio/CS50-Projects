d1493f63933946fd08513f3d31d9ad5d14fdc4b8
holistudio <daniel.bin.lu@gmail.com>
2018-10-11 21:04:07 -0400

    Menu Items Admin setup

--- Project3/orders/admin.py
+++ Project3/orders/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import PizzaMenuItem, SubMenuItem, PastaMenuItem, SaladMenuItem, PlatterMenuItem
+
+class SimpleMenuItemAdmin(admin.ModelAdmin):
+	list_display = ('item_name', 'price')
+	fields = ['item_name', 'price']
+
+class SizeMenuItemAdmin(admin.ModelAdmin):
+	#for items with size
+	list_display = ('item_name', 'size', 'price')
+	fields = ['item_name', 'size', 'price']
+
+class PizzaMenuItemAdmin(admin.ModelAdmin):
+	#for pizzas
+	list_display = ('item_name', 'size', 'topping_sel', 'price')
+	fields = ['item_name', 'size', 'topping_sel', 'price']
+
+
+
+
+admin.site.register(PizzaMenuItem, PizzaMenuItemAdmin)
+admin.site.register(SubMenuItem, SizeMenuItemAdmin)
+admin.site.register(PastaMenuItem, SimpleMenuItemAdmin)
+admin.site.register(SaladMenuItem, SimpleMenuItemAdmin)
+admin.site.register(PlatterMenuItem, SizeMenuItemAdmin)
--- Project3/orders/models.py
+++ Project3/orders/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+#Item Type (Pizza/Sub/Pasta/Salad/Platter)
+PIZZA = 'PZA'
+SUB = 'SUB'
+PASTA = 'PST'
+SALAD = 'SAL'
+PLATTER = 'PLT'
+
+ITEM_TYPE_CHOICES = (
+	(PIZZA, "Pizza"),
+	(SUB, "Sub"),
+	(PASTA, "Pasta"),
+	(SALAD, "Salad"),
+	(PLATTER, "Platter"),
+)
+
+#Size selections
 SMALL = 'S'
 LARGE = 'L'
 
@@ -11,47 +27,37 @@ SIZE_CHOICES = (
 
 # Create your models here.
 class MenuItem(models.Model):
-	#Price
-	price = models.DecimalField(max_digits=5, decimal_places=2)
-
-	#Item Type (Pizza/Sub/Pasta/Salad/Platter)
-	PIZZA = 'PZA'
-	SUB = 'SUB'
-	PASTA = 'PTA'
-	SALAD = 'SAL'
-	PLATTER = 'PLT'
-
-	ITEM_TYPE_CHOICES = (
-		(PIZZA, "Pizza"),
-		(SUB, "Sub"),
-		(PASTA, "Sub"),
-		(SALAD, "Salad"),
-		(PLATTER, "Platter"),
-	)
-
 	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PIZZA);
 
 	item_name = models.CharField(max_length=100);
 
+	#Price
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+
 	#"toString method"
 	def __str__(self):
-		str(f"{self.item_type} - {self.item_name}");
+		return str(f"{self.item_type} - {self.item_name}");
 
 	class Meta:
 		abstract = True
 
 #PizzaMenuItem (inherit MenuItem)
 class PizzaMenuItem(MenuItem):
+	class Meta:
+		verbose_name = "Pizza Menu Item"
+
+	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PIZZA);
+
 	#Pizza Type (Regular/Sicilian)
-	REGULAR = 'RGL'
-	SICILIAN = 'SCL'
+	REGULAR = 'Regular'
+	SICILIAN = 'Sicilian'
 
 	PIZZA_TYPE_CHOICES = (
 		(REGULAR, "Regular"),
 		(SICILIAN, "Sicilian"),
 	)
 
-	item_name = models.CharField(max_length=3, choices = PIZZA_TYPE_CHOICES, default = REGULAR)
+	item_name = models.CharField(max_length=100, choices = PIZZA_TYPE_CHOICES, default = REGULAR)
 
 	#Pizza Size (Small/Large)
 	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
@@ -64,26 +70,38 @@ class PizzaMenuItem(MenuItem):
 		('3',"3 toppings"),
 		('S',"Special")
 	)
-	topping_sel = models.CharField(max_length=1, choices = TOPPING_SEL_CHOICES, default = 'C')
+	topping_sel = models.CharField(max_length=1, choices = TOPPING_SEL_CHOICES, default = 'C', verbose_name="Topping Selection")
 
 #ToppingMenuItem (though it is technically a menu item, a topping doesn't have an associated price or "type")
 class ToppingMenuItem(models.Model):
+	class Meta:
+		verbose_name = "Topping Menu Item"
+
 	#Topping Name
 	name = models.CharField(max_length=100);
 
 class SubMenuItem(MenuItem):
+	class Meta:
+		verbose_name = "Sub Menu Item"
+
+	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = SUB);
 	#Sub Size (Small/Large)
 	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
 
 class SaladMenuItem(MenuItem):
 	class Meta:
-		abstract = False;
+		verbose_name = "Salad Menu Item"
+	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = SALAD);
 
 class PastaMenuItem(MenuItem):
 	class Meta:
-		abstract = False;
+		verbose_name = "Pasta Menu Item"
+	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PASTA);
 
 
 class PlatterMenuItem(MenuItem):
+	class Meta:
+		verbose_name = "Platter Menu Item"
+	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PLATTER);
 	#Platter Size (Small/Large)
 	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
