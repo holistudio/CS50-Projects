0dacad64d1182df3c971114a64e2e9dcf3e1d586
holistudio <daniel.bin.lu@gmail.com>
2018-10-13 13:30:45 -0400

    user login with valid and invalid use cases

--- Project3/orders/views.py
+++ Project3/orders/views.py
@@ -6,23 +6,21 @@ from django.urls import reverse
 # Create your views here.
 
 def index(request):
-    if not request.user.is_authenticated:
-        return render(request, "orders/login.html", {"message": None})
-    context = {
-        "user": request.user
-    }
-    return render(request, "orders/user.html", context)
+	return render(request, "orders/index.html")
 
 def login_view(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, "orders/login.html", {"message": "Invalid credentials."})
+	if request.method == 'GET':
+		return render(request, "orders/login.html")
+	elif request.method == 'POST':
+		username = request.POST["username"]
+		password = request.POST["password"]
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect(reverse("orders:index"))
+		else:
+			return render(request, "orders/login.html", {"message": "Invalid username or password"})
 
 def logout_view(request):
     logout(request)
-    return render(request, "orders/login.html", {"message": "Logged out."})
+    return HttpResponseRedirect(reverse("orders:index"))
