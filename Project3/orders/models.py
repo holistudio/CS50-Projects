6548cffc97b73add092ba10042c8534c02e9b43f
holistudio <daniel.bin.lu@gmail.com>
2018-10-15 17:36:23 -0400

    made models.py more readable

--- Project3/orders/models.py
+++ Project3/orders/models.py
@@ -31,7 +31,6 @@ class MenuItem(models.Model):
 
 	item_name = models.CharField(max_length=100);
 
-	#Price
 	price = models.DecimalField(max_digits=5, decimal_places=2)
 
 	#"toString method"
@@ -43,12 +42,6 @@ class MenuItem(models.Model):
 
 #PizzaMenuItem (inherit MenuItem)
 class PizzaMenuItem(MenuItem):
-	class Meta:
-		verbose_name = "Pizza Menu Item"
-		unique_together = ('item_type','item_name','size','topping_sel')
-
-	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PIZZA);
-
 	#Pizza Type (Regular/Sicilian)
 	REGULAR = 'Regular'
 	SICILIAN = 'Sicilian'
@@ -56,12 +49,7 @@ class PizzaMenuItem(MenuItem):
 	PIZZA_TYPE_CHOICES = (
 		(REGULAR, "Regular"),
 		(SICILIAN, "Sicilian"),
-	)
-
-	item_name = models.CharField(max_length=100, choices = PIZZA_TYPE_CHOICES, default = REGULAR)
-
-	#Pizza Size (Small/Large)
-	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
+	)	
 
 	#Topping Selection	(Cheese 0, 1 topping 1, 2 toppings 2, 3 toppings 3, Special 4)
 	TOPPING_SEL_CHOICES = (
@@ -71,46 +59,57 @@ class PizzaMenuItem(MenuItem):
 		('3',"3 toppings"),
 		('S',"Special")
 	)
+	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PIZZA);
+
+	item_name = models.CharField(max_length=100, choices = PIZZA_TYPE_CHOICES, default = REGULAR)
+
+	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
+
 	topping_sel = models.CharField(max_length=1, choices = TOPPING_SEL_CHOICES, default = 'C', verbose_name="Topping Selection")
+
+	class Meta:
+			verbose_name = "Pizza Menu Item"
+			unique_together = ('item_type','item_name','size','topping_sel')
 
 #ToppingMenuItem (though it is technically a menu item, a topping doesn't have an associated price or "type")
 class ToppingMenuItem(models.Model):
-	class Meta:
-		verbose_name = "Topping Menu Item"
-
 	#Topping Name
 	item_name = models.CharField(max_length=100, unique = True);
+
+	class Meta:
+		verbose_name = "Topping Menu Item"
 
 	#"toString method"
 	def __str__(self):
 		return str(f"TP - {self.item_name}");
 
 class SubMenuItem(MenuItem):
+	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = SUB);
+
+	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
+
 	class Meta:
 		verbose_name = "Sub Menu Item"
 		unique_together = ('item_type','item_name','size')
 
-	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = SUB);
-	#Sub Size (Small/Large)
-	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
-
 class SaladMenuItem(MenuItem):
+	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = SALAD);
 	class Meta:
 		verbose_name = "Salad Menu Item"
 		unique_together = ('item_type','item_name')
-	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = SALAD);
 
 class PastaMenuItem(MenuItem):
+	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PASTA);
 	class Meta:
 		verbose_name = "Pasta Menu Item"
 		unique_together = ('item_type','item_name')
-	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PASTA);
 
 
 class PlatterMenuItem(MenuItem):
+	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PLATTER);
+
+	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
+
 	class Meta:
 		verbose_name = "Platter Menu Item"
 		unique_together = ('item_type','item_name','size')
-	item_type = models.CharField(max_length=3, choices = ITEM_TYPE_CHOICES, default = PLATTER);
-	#Platter Size (Small/Large)
-	size = models.CharField(max_length=1, choices = SIZE_CHOICES, default = SMALL)
