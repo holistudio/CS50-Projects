d3df6f7105a71254ca7df834d3aa67b6741f7600
holistudio <daniel.bin.lu@gmail.com>
2018-10-15 14:58:25 -0400

    user registration

--- Project3/orders/urls.py
+++ Project3/orders/urls.py
@@ -6,6 +6,7 @@ app_name = 'orders'
 
 urlpatterns = [
 	path("", views.index, name="index"),
-	path("login", views.login_view, name="login"),
-	path("logout", views.logout_view, name="logout")
+	path("login/", views.login_view, name="login"),
+	path("logout/", views.logout_view, name="logout"),
+	path("register/", views.register_view, name="register")
 ]
--- Project3/orders/views.py
+++ Project3/orders/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 
@@ -24,3 +26,30 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("orders:index"))
+
+def register_view(request):
+	if request.method =="POST":
+
+		# Get form information.
+		username = request.POST["username"]
+		email = request.POST["email"]
+		password = request.POST["password"]
+		confirm = request.POST["pw-conf"]
+
+		#make sure password match
+		if password != confirm:
+			return render(request, "orders/register.html", {"message": "Passwords do not match"})
+		#make sure username does not already exist
+		try:
+			user = User.objects.create_user(username, email, password)
+		except IntegrityError as e:
+			if(str(e.__cause__)=='UNIQUE constraint failed: auth_user.username'):
+				return render(request, "orders/register.html", {"message": "Username already exists"})
+			else:
+				return render(request, "orders/register.html", {"message": "Invalid inputs"})
+		else:
+			#add username and password to database
+			user.save()
+			return HttpResponseRedirect(reverse("orders:index"))	
+	else:
+		return render(request, "orders/register.html")
