e193f2083ad1b6c15e7b4fe3773dc79d75af41e3
holistudio <daniel.bin.lu@gmail.com>
2018-10-15 17:33:11 -0400

    testing comments

--- Project3/orders/tests.py
+++ Project3/orders/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+#blank inputs
+
+#valid character length
+
+#character length exceeded
+
+#normal prices
+
+#price digits exceeded
+
+#negative prices not allowed
+
+#invalid choices
+
+#valid choices
