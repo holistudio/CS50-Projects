eb59a305e98bf96a7d844a24b33a48e665ac9084
holistudio <daniel.bin.lu@gmail.com>
2018-10-16 17:12:50 -0400

    model tests

--- Project3/orders/tests.py
+++ Project3/orders/tests.py
@@ -1,8 +1,28 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
+
+
+from .models import PizzaMenuItem
 
 # Create your tests here.
+class PizzaMenuItemModelTests(TestCase):
+
+	def test_duplicates(self):
+		p1 = PizzaMenuItem(item_name = 'Regular', size = 'L', topping_sel = '1',price = 2.00);
+		p1.save()
+		p2 = PizzaMenuItem(item_name = 'Regular', size = 'L', topping_sel = '1', price = 1.00)
+		with self.assertRaises(Exception) as raised:
+			p2.save()
+		self.assertEqual(IntegrityError, type(raised.exception))
+	
+	#negative prices not allowed
+	def test_negative_price(self):
+		price = -0.01
+		p = PizzaMenuItem(price = price);
+		self.assertRaises(IntegrityError, p.save());
 
 #blank inputs
+
 
 #valid character length
 
@@ -11,8 +31,6 @@ from django.test import TestCase
 #normal prices
 
 #price digits exceeded
-
-#negative prices not allowed
 
 #invalid choices
 
