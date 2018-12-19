0a29b28ed80a3acd4fab15df961fb3bc478b0e42
holistudio <daniel.bin.lu@gmail.com>
2018-12-19 16:53:39 -0500

    added topping list to context

--- Project3/orders/views.py
+++ Project3/orders/views.py
@@ -19,6 +19,7 @@ def index(request):
 	pasta_list = PastaMenuItem.objects.order_by('price');
 	salad_list = SaladMenuItem.objects.order_by('price');
 	platter_list = PlatterMenuItem.objects.order_by('item_name', '-size');
+	topping_list = ToppingMenuItem.objects;
 	template = loader.get_template('orders/index.html')
 	context = {
 		'pizza_list': pizza_list,
@@ -27,6 +28,7 @@ def index(request):
 		'pasta_list': pasta_list,
 		'salad_list': salad_list,
 		'platter_list': platter_list,
+		'topping_list':topping_list,
 	}
 	return HttpResponse(template.render(context, request))
 
