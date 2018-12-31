819587f8a3837f601f1f147d1cbbee0704b31b94
holistudio <daniel.bin.lu@gmail.com>
2018-12-30 19:12:47 -0500

    checkout url added

--- Project3/orders/urls.py
+++ Project3/orders/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 	path("menuitem/", views.item_display, name="item_display"),
 	path("additemtocart/", views.add_item_to_cart, name="add_item_to_cart"),
 	path("shoppingcart/", views.shopping_cart, name="shopping_cart"),
+	path("checkout/", views.check_out, name="check_out"),
 	path("login/", views.login_view, name="login"),
 	path("logout/", views.logout_view, name="logout"),
 	path("register/", views.register_view, name="register")
